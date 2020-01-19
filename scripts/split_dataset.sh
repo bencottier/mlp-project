0dd67a1f2f1ffea9c564c904387b8fd18abaaba9
Ben Cottier <bcottier2@gmail.com>
2020-01-19 23:06:41 +0000

    Update split_dataset.sh for extended options

--- scripts/split_dataset.sh
+++ scripts/split_dataset.sh
@@ -1,6 +1,8 @@
 # First run 
 #     export MATH_DATASET=<absolute path to dataset>
 
-python split_dataset.py -i ${MATH_DATASET}/train-easy -o ${MATH_DATASET}/train-easy-split
-python split_dataset.py -i ${MATH_DATASET}/train-medium -o ${MATH_DATASET}/train-medium-split
-python split_dataset.py -i ${MATH_DATASET}/train-hard -o ${MATH_DATASET}/train-hard-split
+python split_dataset.py -i ${MATH_DATASET}/train-easy -o ${MATH_DATASET}/train-easy-split --train_split 0.9 --token char
+python split_dataset.py -i ${MATH_DATASET}/train-medium -o ${MATH_DATASET}/train-medium-split --train_split 0.9 --token char
+python split_dataset.py -i ${MATH_DATASET}/train-hard -o ${MATH_DATASET}/train-hard-split --train_split 0.9 --token char
+python split_dataset.py -i ${MATH_DATASET}/interpolate -o ${MATH_DATASET}/interpolate-split --token char
+python split_dataset.py -i ${MATH_DATASET}/extrapolate -o ${MATH_DATASET}/extrapolate-split --token char
