d457ee3585bb8e694297ab297dc8d67df3e77313
Ben Cottier <bcottier2@gmail.com>
2020-01-24 15:01:33 +0000

    Add metrics script

---
+++ scripts/metrics.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+metrics.py
+
+Compute performance metrics on model predictions.
+"""
+import argparse
+import nltk
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ref', type=str, default=None,
+                        help='Reference filename: contains ground-truth sequences')
+    parser.add_argument('hyp', type=str, default=None,
+                        help='Hypothesis filename: contains predicted sequences')
+    args = parser.parse_args()
+
+    return args
+
+
+def main(ref_file, hyp_file):
+    print(f'Reference file: {ref_file}')
+    print(f'Hypothesis file: {hyp_file}')
+
+    with open(ref_file, 'r') as f:
+        ref_lines = f.readlines()
+
+    with open(hyp_file, 'r') as f:
+        hyp_lines = f.readlines()
+
+    bleus = np.zeros(len(ref_lines))
+    accs = np.zeros(len(ref_lines))
+    for i, (ref_line, hyp_line) in enumerate(zip(ref_lines, hyp_lines)):
+        ref = ref_line.strip().split(' ')
+        refs = [ref]
+        hyp = hyp_line.strip().split(' ')
+        bleu = nltk.translate.bleu_score.sentence_bleu(refs, hyp)
+        bleus[i] = bleu
+        accs[i] = np.allclose(bleu, 1.0)
+        print(i)
+        print(ref)
+        print(hyp)
+        print(bleu)
+        print('')
+
+    # TODO better aggregation for BLEU than an average?
+    print(f'AVG BLEU: {bleus.mean():.4f}')
+    print(f'AVG ACC: {accs.mean():.4f}')
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args.ref, args.hyp)
