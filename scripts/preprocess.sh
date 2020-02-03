eea79e78b8d372872deefcfc0ea51e2bf008ec09
Ben Cottier <bcottier2@gmail.com>
2020-02-03 22:16:50 +0000

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -3,10 +3,6 @@
 # WARNING: CHECK ALL VARIABLES AND COMMANDS CAREFULLY
 
 DATAPATH="data/mathematics_dataset-v1.0/train-easy-split/calculus__differentiate_"
-EXPNAME="calculus__differentiate_10000"
+EXPDIR="data/bin/calculus__differentiate"
 
-# onmt_preprocess -train_src data/mathematics_dataset-v1.0/train-easy-split/algebra__linear_1d_src_train.txt -train_tgt data/mathematics_dataset-v1.0/train-easy-split/algebra__linear_1d_tgt_train.txt -valid_src data/mathematics_dataset-v1.0/train-easy-split/algebra__linear_1d_src_valid.txt -valid_tgt data/mathematics_dataset-v1.0/train-easy-split/algebra__linear_1d_tgt_valid.txt -save_data data/demo/algebra__linear_1d/data
-
-# onmt_preprocess -train_src ${DATAPATH}src_train.txt -train_tgt ${DATAPATH}tgt_train.txt -valid_src ${DATAPATH}src_valid.txt -valid_tgt ${DATAPATH}tgt_valid.txt -save_data data/demo/$EXPNAME/data
-
-onmt_preprocess -train_src ${DATAPATH}src_train_10000.txt -train_tgt ${DATAPATH}tgt_train_10000.txt -valid_src ${DATAPATH}src_valid_1000.txt -valid_tgt ${DATAPATH}tgt_valid_1000.txt -save_data data/demo/$EXPNAME/data
+onmt_preprocess -train_src ${DATAPATH}src_train.txt -train_tgt ${DATAPATH}tgt_train.txt -valid_src ${DATAPATH}src_valid.txt -valid_tgt ${DATAPATH}tgt_valid.txt -save_data ${EXPDIR}/data
