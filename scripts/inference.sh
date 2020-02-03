555d4a9d643e857418dd1e167a0f35dc7fae172d
Ben Cottier <bcottier2@gmail.com>
2020-02-03 22:16:54 +0000

    Update inference.sh

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -3,13 +3,12 @@
 # WARNING: CHECK ALL VARIABLES AND COMMANDS CAREFULLY
 
 TASK="calculus__differentiate"
-MODEL="size-100"
+MODEL="-size-100"
 CHECKPOINT="85000"
-EXPDIR="data/demo/calculus__differentiate"
+EXPDIR="exp/calculus__differentiate"
 
-# onmt_translate -model demo-model_step_250.pt -src data/mathematics_dataset-v1.0/interpolate-split/algebra__linear_1d_src_test.txt -output data/demo/pred.txt -replace_unk -verbose
+# Validation
+onmt_translate -model ${EXPDIR}/model${MODEL}_step_${CHECKPOINT}.pt -src data/mathematics_dataset-v1.0/train-easy-split/${TASK}_src_valid.txt -output data/demo/${TASK}/pred_valid_${CHECKPOINT}.txt -replace_unk -verbose
 
-# onmt_translate -model ${EXPDIR}/model-${MODEL}_step_${CHECKPOINT}.pt -src data/mathematics_dataset-v1.0/interpolate-split/${TASK}_src_test.txt -output data/demo/${TASK}/pred_${CHECKPOINT}.txt -replace_unk -verbose
-
-# Rerun on training data
-onmt_translate -model ${EXPDIR}/model-${MODEL}_step_${CHECKPOINT}.pt -src data/mathematics_dataset-v1.0/train-easy-split/${TASK}_src_valid_1000.txt -output data/demo/${TASK}/pred_valid_${CHECKPOINT}.txt -replace_unk -verbose
+# Test
+# onmt_translate -model ${EXPDIR}/model${MODEL}_step_${CHECKPOINT}.pt -src data/mathematics_dataset-v1.0/interpolate-split/${TASK}_src_test.txt -output data/demo/${TASK}/pred_test_${CHECKPOINT}.txt -replace_unk -verbose
